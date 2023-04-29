6957344fdae0da6ca2c312535decb178dd740377
Boadu Gideon <kwesijnr505050@gmail.com>
2023-04-29 13:44:16 +0000

    .

--- php/dashboard.php
+++ php/dashboard.php
@@ -22,8 +22,7 @@ $lname = $row['lname'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home-page</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Home-page</title>       
     <link rel="stylesheet" href="dashboard.css">
     <link rel="stylesheet" href="responsive.css">
     <link rel="shortcut icon" href="images/logo.jpeg" type="image/x-icon">
